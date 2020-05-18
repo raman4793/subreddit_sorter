ff261427ccd5a5fb8a1ff82985a5efc5b2dabda8
ram <ramachandran.r@attinadsoftware.com>
2020-05-18 22:59:29 +0530

    added test for post score calculation

--- score_calculator/simple_post_score_calculator.py
+++ score_calculator/simple_post_score_calculator.py
@@ -1,5 +1,7 @@
+import numpy
 import pandas as pd
 
+from exceptions.exceptions import InvalidArgumentValue
 from .base_calculator import BaseCalculator
 
 
@@ -8,5 +10,11 @@ class SimplePostScoreCalculator(BaseCalculator):
         super().__init__(*args, **kwargs)
 
     def score(self, *, post, comments_data_frame: pd.DataFrame):
+        if type(post) is not dict:
+            raise InvalidArgumentValue("post should be a dictionary")
+        if type(comments_data_frame) is not pd.DataFrame:
+            raise InvalidArgumentValue("comments data frame needs a pandas data frame")
+        if comments_data_frame.dtypes["ups"] != numpy.float:
+            raise InvalidArgumentValue("comments data frame contains invalid data")
         post["score"] = sum(comments_data_frame["score"] / comments_data_frame["score"].count())
         return post
--- tests/test_comment_score_calculator.py
+++ tests/test_comment_score_calculator.py
@@ -3,7 +3,7 @@ import unittest
 import pandas
 
 from exceptions.exceptions import InvalidArgumentValue
-from score_calculator import get_comment_score_calculator
+from score_calculator import get_comment_score_calculator, get_post_score_calculator
 from scraper import FileScrapper
 from utils import model_to_data_frame
 
@@ -27,12 +27,41 @@ class TestCommentScoreCalculator(unittest.TestCase):
 
     def test_valid_data_frame(self):
         values = [
-            {"ups": 15.0, "score": -0.605920},
-            {"ups": 10.0, "score": 0.572258},
-            {"ups": 5.0, "score": 1.413813},
-            {"ups": 3.0, "score": -1.110853},
-            {"ups": 0.0, "score": -0.269298}
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 9.9, "score": 0.3618650184255208},
+            {"ups": 8, "score": -1.7867085284760074}
         ]
         data_frame = model_to_data_frame(values)
         score = self.score_calculator.score(comments_data_frame=data_frame)
-        pandas.testing.assert_series_equal(left=data_frame["score"], right=score["score"])
+        pandas.testing.assert_frame_equal(left=model_to_data_frame(values), right=score)
+
+
+class TestPostScoreCalculator(unittest.TestCase):
+    def setUp(self):
+        self.file_scraper = FileScrapper(cache_folder="cache")
+        self.score_calculator = get_post_score_calculator()
+
+    def test_invalid_data(self):
+        values = ["asdf", b"asdfasdf", 5, {"a": "asdf"}, [1, "asdf"]]
+        for value in values:
+            arguments = {"comments_data_frame": value, "post": "abc"}
+            self.assertRaises(InvalidArgumentValue, self.score_calculator.score, **arguments)
+
+    def test_invalid_data_frame(self):
+        values = [{"ups": "abc"}]
+        data_frame = model_to_data_frame(values)
+        arguments = {"comments_data_frame": data_frame, "post": "ac"}
+        self.assertRaises(InvalidArgumentValue, self.score_calculator.score, **arguments)
+
+    def test_valid_data_frame(self):
+        comments = [
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 9.9, "score": 0.3618650184255208}
+        ]
+        data_frame = model_to_data_frame(comments)
+        score = self.score_calculator.score(post={}, comments_data_frame=data_frame)
+        self.assertDictEqual(score, {'score': 0.44667713211900184})
