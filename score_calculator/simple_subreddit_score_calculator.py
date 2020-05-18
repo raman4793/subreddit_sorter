8b2c58619ea7653bf2dc75046c0a4028c0422594
ram <ramachandran.r@attinadsoftware.com>
2020-05-18 23:01:50 +0530

    added test for subreddit score calculation

--- score_calculator/simple_subreddit_score_calculator.py
+++ score_calculator/simple_subreddit_score_calculator.py
@@ -1,5 +1,7 @@
+import numpy
 import pandas as pd
 
+from exceptions.exceptions import InvalidArgumentValue
 from .base_calculator import BaseCalculator
 
 
@@ -8,5 +10,11 @@ class SimpleSubredditScoreCalculator(BaseCalculator):
         super().__init__(*args, **kwargs)
 
     def score(self, *, subreddit, posts_data_frame: pd.DataFrame):
+        if type(subreddit) is not dict:
+            raise InvalidArgumentValue("post should be a dictionary")
+        if type(posts_data_frame) is not pd.DataFrame:
+            raise InvalidArgumentValue("comments data frame needs a pandas data frame")
+        if posts_data_frame.dtypes["ups"] != numpy.float:
+            raise InvalidArgumentValue("comments data frame contains invalid data")
         subreddit["score"] = sum(posts_data_frame["score"] / posts_data_frame["score"].count())
         return subreddit
--- tests/test_comment_score_calculator.py
+++ tests/test_comment_score_calculator.py
@@ -3,7 +3,7 @@ import unittest
 import pandas
 
 from exceptions.exceptions import InvalidArgumentValue
-from score_calculator import get_comment_score_calculator, get_post_score_calculator
+from score_calculator import get_comment_score_calculator, get_post_score_calculator, get_sub_score_calculator
 from scraper import FileScrapper
 from utils import model_to_data_frame
 
@@ -64,4 +64,33 @@ class TestPostScoreCalculator(unittest.TestCase):
         ]
         data_frame = model_to_data_frame(comments)
         score = self.score_calculator.score(post={}, comments_data_frame=data_frame)
+        self.assertDictEqual(score, {'score': 0.44667713211900184})
+
+
+class TestSubredditScoreCalculator(unittest.TestCase):
+    def setUp(self):
+        self.file_scraper = FileScrapper(cache_folder="cache")
+        self.score_calculator = get_sub_score_calculator()
+
+    def test_invalid_data(self):
+        values = ["asdf", b"asdfasdf", 5, {"a": "asdf"}, [1, "asdf"]]
+        for value in values:
+            arguments = {"posts_data_frame": value, "subreddit": "abc"}
+            self.assertRaises(InvalidArgumentValue, self.score_calculator.score, **arguments)
+
+    def test_invalid_data_frame(self):
+        values = [{"ups": "abc"}]
+        data_frame = model_to_data_frame(values)
+        arguments = {"posts_data_frame": data_frame, "subreddit": "ac"}
+        self.assertRaises(InvalidArgumentValue, self.score_calculator.score, **arguments)
+
+    def test_valid_data_frame(self):
+        subreddits = [
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 10.0, "score": 0.47494783668349555},
+            {"ups": 9.9, "score": 0.3618650184255208}
+        ]
+        data_frame = model_to_data_frame(subreddits)
+        score = self.score_calculator.score(subreddit={}, posts_data_frame=data_frame)
         self.assertDictEqual(score, {'score': 0.44667713211900184})
