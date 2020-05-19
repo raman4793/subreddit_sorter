2f402663dc333e19eca71b5d0a7187f12ae98033
ram <ramachandran.r@attinadsoftware.com>
2020-05-19 17:44:20 +0530

    added fixed scheduler to start

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 python3 -m unittest tests.test_configuration
-PYTHONPATH=$(pwd) luigi --module tasks.score_calculators.score_calculator RangeDaily --of CalculateScore --start 2020-01-01 --of-params '{"top_n_subreddits": "$subreddit_limit", "top_n_comments": "$comments_limit", "top_n_posts": "$posts_limit"}' --local-scheduler
+PYTHONPATH=$(pwd) luigi --module tasks.score_calculators.score_calculator CalculateScore --top-n-subreddits $subreddit_limit --top-n-posts $posts_limit --top-n-comments $comments_limit --local-scheduler
